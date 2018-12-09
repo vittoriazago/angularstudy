408084ffbe1d6ecdbe32a6f318ed3c26a6f087c2
Vittoria Zago <vittoriazago1@hotmail.com>
2018-12-09 13:16:10 -0200

    Testando data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  description = 'Leão';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
+}
 }
